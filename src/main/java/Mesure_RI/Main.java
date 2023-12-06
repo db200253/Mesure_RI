31e6de4c7f52420d54799be9fba7d7a6dcb8538d
Baptiste Duvieu <baptiste.duvieu1@etu.univ-lehavre.fr>
2023-12-06 08:24:42 +0100

    Distribution des distances

--- src/main/java/Mesure_RI/Main.java
+++ src/main/java/Mesure_RI/Main.java
@@ -3,10 +3,12 @@ package Mesure_RI;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Locale;
-
 import org.graphstream.algorithm.Toolkit;
+import org.graphstream.graph.BreadthFirstIterator;
 import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.DefaultGraph;
 import org.graphstream.stream.file.FileSource;
 import org.graphstream.stream.file.FileSourceEdge;
@@ -35,6 +37,7 @@ public class Main {
       System.out.println();
       printIfConnnected();
       getDegreeDistribution();
+      getDistances();
       fs.removeSink(g);
     }
   }
@@ -68,6 +71,9 @@ public class Main {
       System.out.println("Le réseau n'est pas connexe");
   }
   
+  /**
+   * Question 4 sur la distribution des degrés
+   */
   private static void getDegreeDistribution() {
     
     String outputFile = "src/main/ressources/degreeDistribution.csv";
@@ -85,6 +91,57 @@ public class Main {
     } catch (IOException e) {
       
         e.printStackTrace();
+    }
+  }
+  
+  /**
+   * Question 5 sur la distribution des distances
+   */
+  private static void getDistances() {
+    
+    float[] distances = new float[1000];
+    String outputFile = "src/main/ressources/distanceDistribution.csv";
+    float distMoy = 0;
+    
+    ArrayList<Node> echantillon = new ArrayList<Node>();
+    
+    while (echantillon.size() != 1000) {
+      
+        Node noeud = Toolkit.randomNode(g);
+        
+        if(!(echantillon.contains(noeud))) echantillon.add(noeud);
+    }
+    
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+
+      for(int i = 0; i < 1000; ++i) {
+        
+        BreadthFirstIterator bfs = new BreadthFirstIterator(echantillon.get(i));
+        
+        float tmp = 0;
+        
+        for(int j = 0; j < 1000; ++j) {
+          
+          bfs.next();
+          tmp += bfs.getDepthMax();
+        }
+        
+        tmp /= 1000.0;
+        distances[i] = tmp;
+        distMoy += tmp;
+      }
+      
+      distMoy /= 1000;
+      System.out.println("Distance moyenne du réseau : " + distMoy);
+      
+      for (int i = 0; i < distances.length; i++) {
+        
+        double normalizedValue = (double) distances[i];
+        writer.write(String.format(Locale.US, "%.1f", normalizedValue) + "\n");
+      }
+    } catch (IOException e) {
+      
+      e.printStackTrace();
     }
   }
 }
