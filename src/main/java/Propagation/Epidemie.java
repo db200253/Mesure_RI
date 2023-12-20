c6d6c143b3d422830b082ef52dbfa4481a5b1a48
Baptiste Duvieu <baptiste.duvieu1@etu.univ-lehavre.fr>
2023-12-20 11:03:11 +0100

    simul

---
+++ src/main/java/Propagation/Epidemie.java
@@ -0,0 +1,119 @@
+package Propagation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+
+public class Epidemie {
+  
+    final int NOTHING = 0;
+    final int ALEA = 1;
+    final int SELECT = 2;
+    final double TRANSMISSION_PROBABILITY = 1/7;
+    final double RECOVERY_PROBABILITY = 1/14;
+
+    private static Graph g;
+    @SuppressWarnings("unused")
+    private int immune;
+    private List<Node> contamine;
+
+    public Epidemie(Graph graph, int immune) {
+        
+        g = graph;
+        
+        switch(immune) {
+          case 0 : this.immune = NOTHING;
+          case 1 : this.immune = ALEA;
+          case 2 : this.immune = SELECT;
+        }
+        
+        contamine = new ArrayList<>();
+    }
+
+    public void runSimulation(int initialInfectedNodes, int simulationSteps) {
+        initializeSimulation(initialInfectedNodes);
+
+        for (int step = 0; step < simulationSteps; ++step) {
+            updateEpidemicState();
+            System.out.println("Step " + step + ": Infected Count = " + countInfected());
+        }
+    }
+
+    private void initializeSimulation(int initialInfectedNodes) {
+        Random random = new Random();
+
+        for (int i = 0; i < initialInfectedNodes; ++i) {
+            int randomNode = random.nextInt(g.getNodeCount());
+            g.getNode(randomNode).setAttribute("infected", true);
+            contamine.add(g.getNode(randomNode));
+        }
+        
+        for (Node node : g) {
+            
+            if(!(node.hasAttribute("infected"))) {
+              
+              node.setAttribute("infected", false);
+            }
+        }
+    }
+
+    private void updateEpidemicState() {
+        for (Node node : g) {
+            boolean infected = (boolean) node.getAttribute("infected");
+
+            if (infected) {
+                for (Node neighbor : getNeighbours(node)) {
+                  
+                    boolean neighborInfected = (boolean) neighbor.getAttribute("infected");
+                    
+                    if (!neighborInfected && getRand() < TRANSMISSION_PROBABILITY) {
+                      
+                        neighbor.setAttribute("infected", true);
+                        contamine.add(neighbor);
+                    }
+                }
+
+                if (getRand() < RECOVERY_PROBABILITY) {
+                  
+                    node.setAttribute("infected", false);
+                    contamine.remove(node);
+                }
+            }
+        }
+    }
+
+    private int countInfected() {
+        
+      return contamine.size();
+    }
+    
+    private static List<Node> getNeighbours(Node n) {
+      
+      List<Edge> le = n.leavingEdges().collect(Collectors.toList());
+      List<Node> voisins = new ArrayList<>();
+      
+      for(Edge e : le) {
+        
+        voisins.add(e.getOpposite(n));
+      }
+      
+      return voisins;
+    }
+    
+    public static double getRand() {
+      
+      return Math.random();
+    }
+
+    public static void go(Graph g, int step, int immune) {
+
+        System.out.println("coucou");
+        Epidemie simulation = new Epidemie(g, immune);
+        simulation.runSimulation(1, step);
+    }
+}
---
+++ src/main/java/Propagation/Main.java
@@ -0,0 +1,87 @@
+package Propagation;
+
+import java.io.IOException;
+
+import org.graphstream.algorithm.Toolkit;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.DefaultGraph;
+import org.graphstream.stream.file.FileSource;
+import org.graphstream.stream.file.FileSourceEdge;
+
+public class Main {
+  
+  static Graph g = new DefaultGraph("Reseau");
+ 
+  public static void main(String[] args) {
+    
+    System.setProperty("org.graphstream.ui", "swing");
+    String filePath = "src/main/ressources/com-dblp.ungraph.txt";
+    FileSource fs = new FileSourceEdge();
+    
+    fs.addSink(g);
+    
+    try {
+      
+      fs.readAll(filePath);
+    } catch(IOException e) {
+      
+      e.printStackTrace();
+    } finally {
+      
+      System.out.println("Graphe experimental : ");
+      printUsefullDatas();
+      System.out.println();
+      getet();
+      System.out.println("------------------------------------------------");
+      simulEpid();
+      fs.removeSink(g);
+    }
+  }
+  
+  /**
+   * Question 1
+   * Taux de propagation : proba de transmission (1/7) / taux de guérison (2/30) ~ 2
+   * Seuil épidémique : k/k² = 6.62.../144.00... = 0.045...
+   * Seuil théorique : 1/k+1 => 1/7.62... = 0,1311963
+   * Seuil épidémique > taux de propagation donc la maladie progresse
+   */
+  
+  private static void printUsefullDatas() {
+    
+    int nbNodes = g.getNodeCount();
+    int nbEdges = g.getEdgeCount();
+    double avgDegree = Toolkit.averageDegree(g);
+    double clusterCoef = Toolkit.averageClusteringCoefficient(g);
+    
+    System.out.println("Nombre de noeuds dans le graphe : " + nbNodes);
+    System.out.println("Nombre de liens dans le graphe : " + nbEdges);
+    System.out.println("Degré moyen du graphe : " + avgDegree);
+    System.out.println("Coefficient de clustering moyen du graphe : " + clusterCoef);
+  }
+  
+  private static void getet() {
+    
+    int[] distrib = Toolkit.degreeDistribution(g);
+    
+    double r = 0.0;
+    double d = 0.0;
+    
+    for(int i = 1; i < distrib.length; ++i) {
+      
+      r += i * i * distrib[i];
+      d += distrib[i];
+    }
+    
+    double avgDegree = Toolkit.averageDegree(g);
+    double sk = r/d;
+    
+    System.out.println("<k²> = " + sk);
+    System.out.println("Seuil épidémique = " + avgDegree/sk);
+  }
+  
+  private static void simulEpid() {
+    
+    System.out.println("couco");
+    Epidemie.go(g, 12, 0);
+  }
+}
